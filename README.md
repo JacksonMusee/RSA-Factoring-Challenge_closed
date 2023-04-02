6319558b385a3d89f5cf50cd1ddd4cdcae761b08
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-02 00:37:18 -0700

    Push

---
+++ README.md
@@ -0,0 +1,1 @@
+The factorization problem
---
+++ factorial.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - see code
+ *
+ *@argc: arg
+ *@argv: arg
+ *Return: 0 for success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int num;
+	int i;
+	(void)(argc);
+
+	num = atoi(argv[1]);
+
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+		printf("%d=%d*%d\n",num, num / i, i);
+		exit(0);
+		}
+	}
+
+	return (0);
+}
